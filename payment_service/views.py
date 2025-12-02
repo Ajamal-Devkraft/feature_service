e9326cd0f144728bf2ea534fa803602db14f01a8
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-02 17:50:26 +0530

    fixed issue

--- payment_service/views.py
+++ payment_service/views.py
@@ -2,18 +2,17 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from payment_service.models import LeadData
 from rest_framework.response import Response
+from django.db import transaction
 # Create your views here.
 
 class ProcessedLeadData(APIView):
 
     def post(self,request):
         lead_id = request.data.get("lead_id")
-        if LeadData.objects.filter(lead_id=lead_id).exists():
-            lead_data = LeadData.objects.filter(lead_id=lead_id).last()
-            status = int(lead_data.status)  + 1
-            LeadData.objects.create(lead_id=lead_id, status=status)
-        else:
-            lead_data = LeadData.objects.create(lead_id=lead_id, status=1)
+        with transaction.atomic():
+            last = LeadData.objects.filter(lead_id=lead_id).order_by('-id').first()
+            status = 1 if last is None else last.status + 1
+            lead = LeadData.objects.create(lead_id=lead_id, status=status)
         return Response({"lead_id": lead_id})
 
 
