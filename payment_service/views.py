b3e902ea90e133346048aeefaa3cd7b9e2565eb7
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-02 15:48:39 +0530

    fixed update issue

--- payment_service/views.py
+++ payment_service/views.py
@@ -9,8 +9,13 @@ class ProcessedLeadData(APIView):
     def post(self,request):
         lead_id = request.data.get("lead_id")
         status = request.data.get("status")
-        lead_data = LeadData.objects.create(lead_id=lead_id, status=status)
-        return Response({"lead_id": lead_id, "status": lead_data.status})
+        if LeadData.objects.filter(lead_id=lead_id).exists():
+            lead_data = LeadData.objects.get(lead_id=lead_id)
+            lead_data.status = status
+            lead_data.save()
+        else:
+            lead_data = LeadData.objects.create(lead_id=lead_id, status=status)
+        return Response({"lead_id": lead_id, "status": status})
 
 
             
