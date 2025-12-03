9d83423a0575c7895452df86194c363eb3075c79
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-03 10:53:05 +0530

    fixed raceround condition

--- payment_service/views.py
+++ payment_service/views.py
@@ -3,15 +3,23 @@ from rest_framework.views import APIView
 from payment_service.models import LeadData
 from rest_framework.response import Response
 from django.db import transaction
+from django.db.models import F
 # Create your views here.
 
 class ProcessedLeadData(APIView):
 
     def post(self,request):
         lead_id = request.data.get("lead_id")
-        last = LeadData.objects.filter(lead_id=lead_id).order_by('-id').first()
-        status = 1 if last is None else int(last.status) + 1
-        lead = LeadData.objects.create(lead_id=lead_id, status=status)
+        with transaction.atomic():
+            # last = LeadData.objects.filter(lead_id=lead_id).order_by('-id').first()
+            # status = 1 if last is None else int(last.status) + 1
+            # lead = LeadData.objects.create(lead_id=lead_id, status=status)
+            last = (LeadData.objects.select_for_update().filter(lead_id=lead_id).order_by('-id').first())
+            status = 1 if last is None else last.status + 1
+            lead = LeadData.objects.create(
+                lead_id=lead_id,
+                status=status
+            )
         return Response({"lead_id": lead_id,"id":lead.id, "status":status})
 
 
