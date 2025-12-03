38ebe16404c601d204a321a162323684365e2b8e
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-03 13:39:31 +0530

    added source column

--- payment_service/models.py
+++ payment_service/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class LeadData(models.Model):
     lead_id = models.CharField(max_length=255)
     status = models.CharField(max_length=255, default="Failed")
+    source = models.CharField(max_length=255, default="")
--- payment_service/views.py
+++ payment_service/views.py
@@ -10,16 +10,17 @@ class ProcessedLeadData(APIView):
 
     def post(self,request):
         lead_id = request.data.get("lead_id")
-        with transaction.atomic():
-            # last = LeadData.objects.filter(lead_id=lead_id).order_by('-id').first()
-            # status = 1 if last is None else int(last.status) + 1
-            # lead = LeadData.objects.create(lead_id=lead_id, status=status)
-            last = (LeadData.objects.select_for_update().filter(lead_id=lead_id).order_by('-id').first())
-            status = 1 if last is None else last.status + 1
-            lead = LeadData.objects.create(
-                lead_id=lead_id,
-                status=status
-            )
+        source = request.data.get("source", 'API')
+        # with transaction.atomic():
+        last = LeadData.objects.filter(lead_id=lead_id).order_by('-id').first()
+        status = 1 if last is None else int(last.status) + 1
+        lead = LeadData.objects.create(lead_id=lead_id, status=status, source=source)
+            # last = (LeadData.objects.select_for_update().filter(lead_id=lead_id).order_by('-id').first())
+            # status = 1 if last is None else last.status + 1
+            # lead = LeadData.objects.create(
+            #     lead_id=lead_id,
+            #     status=status
+            # )
         return Response({"lead_id": lead_id,"id":lead.id, "status":status})
 
 
