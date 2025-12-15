55a818b7f82fe1258bb734832a752215207e55af
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-15 11:25:57 +0530

    fixed select for update issue

--- payment_service/views.py
+++ payment_service/views.py
@@ -19,14 +19,21 @@ class ProcessedLeadData(APIView):
         # with r.lock(f"lead-{lead_id}", timeout=5):
         with transaction.atomic():
             qs = LeadData.objects.select_for_update().filter(lead_id=lead_id)
-            list(qs)
             last_record = qs.order_by('-id').first()
-            # status = int(last_record.status) + 1 if last_record else 1
-            # lead = LeadData.objects.create(lead_id=lead_id, status=status, source=source)
-            last_record.status = F('status') + 1
-            last_record.source = source
-            last_record.save()
-            lead = last_record
+
+            if qs:
+                LeadData.objects.filter(id=last_record.id).update(
+                    status=F('status') + 1,
+                    source=source
+                )
+                last_record.refresh_from_db()
+                lead = last_record
+            else:
+                lead = LeadData.objects.create(
+                    lead_id=lead_id,
+                    status=1,
+                    source=source
+                )
         return Response({"lead_id": lead_id,"id":lead.id, "status":last_record.status, "source":last_record.source})
 
 
