017d5130fc11e0c689521fa1e2d357e8f07ffdbc
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-15 11:52:03 +0530

    fixed select for update issue

--- payment_service/views.py
+++ payment_service/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from payment_service.models import LeadData
 from rest_framework.response import Response
-from django.db import transaction, IntegrityError
+from django.db import transaction, connection
 from django.db.models import F
 import redis
 from django.conf import settings
@@ -18,33 +18,32 @@ class ProcessedLeadData(APIView):
         source = request.data.get("source", 'API')
         # with r.lock(f"lead-{lead_id}", timeout=5):
         with transaction.atomic():
-            try:
-                # 1️⃣ Try to create the record (only ONE thread will succeed)
-                lead, created = LeadData.objects.get_or_create(
-                    lead_id=lead_id,
-                    defaults={
-                        "status": 1,
-                        "source": source
-                    }
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT pg_advisory_xact_lock(hashtext(%s))",
+                    [lead_id]
                 )
 
-                # 2️⃣ If record already exists → increment status safely
-                if not created:
-                    LeadData.objects.filter(id=lead.id).update(
-                        status=F("status") + 1,
-                        source=source
-                    )
-                    lead.refresh_from_db()
+            last_record = (
+                LeadData.objects
+                .filter(lead_id=lead_id)
+                .order_by('-id')
+                .first()
+            )
 
-            except IntegrityError:
-                # 3️⃣ Rare race-condition fallback
-                lead = LeadData.objects.select_for_update().get(lead_id=lead_id)
-                LeadData.objects.filter(id=lead.id).update(
+            if last_record:
+                LeadData.objects.filter(id=last_record.id).update(
                     status=F("status") + 1,
                     source=source
                 )
-                lead.refresh_from_db()
-
+                last_record.refresh_from_db()
+                lead = last_record
+            else:
+                lead = LeadData.objects.create(
+                    lead_id=lead_id,
+                    status=1,
+                    source=source
+                )
         return Response({"lead_id": lead_id,"id":lead.id, "status":lead.status, "source":lead.source})
 
 
