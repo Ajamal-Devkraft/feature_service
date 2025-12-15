eae0828e39b656270abd193dcec31b8dd584c7d9
Ajamal Husain <ajamal.husain@kuliza.com>
2025-12-15 12:38:45 +0530

    fixed model issues

--- payment_service/models.py
+++ payment_service/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class LeadData(models.Model):
-    lead_id = models.CharField(max_length=255)
-    status = models.CharField(max_length=255, default="Failed")
+    lead_id = models.CharField(max_length=255, unique=True)
+    status = models.IntegerField(default=1)
     source = models.CharField(max_length=255, default="")
